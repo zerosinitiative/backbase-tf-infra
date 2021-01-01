3ac26eed100006acd3a79daf23b614e765fd27b6
AkshayaPk <ashpeekay23@gmail.com>
2021-01-01 11:34:48 +0530

    Added modifications

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "backbase_table_state_lock" {
   name     = var.table_name
-  hash_key = "LockId"
+  hash_key = "LockID"
   attribute {
     name = "LockID"
     type = "S"
