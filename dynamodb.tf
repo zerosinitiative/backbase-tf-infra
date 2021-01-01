d9a790cad031cc244889e358ad4213de01589d7d
AkshayaPk <ashpeekay23@gmail.com>
2021-01-01 11:37:19 +0530

    Added read and write capacity

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,5 +1,7 @@
 resource "aws_dynamodb_table" "backbase_table_state_lock" {
   name     = var.table_name
+  read_capacity  = 5
+  write_capacity = 5
   hash_key = "LockID"
   attribute {
     name = "LockID"
